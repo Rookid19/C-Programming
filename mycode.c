2aaef8f80173fb2763feae2c1d64186abfac998e
rookid19 <randyodoom19@gmail.com>
2023-04-23 07:39:14 -0700

    Write a script that compiles a C file but does not link

---
+++ mycode.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+/**
+* main - Entry point of the code
+* Return: success (0)
+*/
+int main(void)
+{
+	puts("\"My name is barry allen and i am the fastest man alive");
+	return (0);
+}
